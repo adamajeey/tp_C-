0d32fd3c99cbadb291665f3d22a37ce7ef0ecf08
Mouhamadou Moustapha SARR <msarrmouhamadou@gmail.com>
2024-12-19 17:00:11 +0000

    Complement de l'exercice 1 pour gerer le cas ou l'utilisateur saisi une chaine à la place d'un entier

---
+++ Exercice1/AtHomeExercices/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtHomeExercices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom, prenom, email;
+            Console.WriteLine("Ce programme vous souhaite la bienvenue");
+
+            Console.WriteLine("Veuillez entrer vos informations suivantes s'il vous plaït");
+
+            Console.WriteLine("Nom :");
+            nom=Console.ReadLine();
+            Console.WriteLine("Prenom :");
+            prenom=Console.ReadLine();
+            Console.WriteLine("Eamil :");
+            email = Console.ReadLine();
+
+            Console.WriteLine(string.Format("Bonjour {0} {1}, votre email est {2}",prenom, nom, email));
+            Console.ReadKey();
+        }
+    }
+}
