c2cec0792b9d1d968b27172e3539976aa7f25e30
Mouhamadou Moustapha SARR <msarrmouhamadou@gmail.com>
2024-12-19 16:47:46 +0000

    Ajout des fichier pour le calcul de la factorielle et des remboursements

---
+++ Factoriel/Exercice 5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string rep = "";
+            // Initialisation de rep
+            Console.WriteLine("Ce programme calcule la factorielle d'un nombre."); 
+            do { 
+                int nombre; 
+                long factorielle = 1; 
+                Console.WriteLine("Veuillez saisir un nombre :"); 
+                while (!int.TryParse(Console.ReadLine(), out nombre)) { 
+
+                    Console.WriteLine("Entrée invalide. Veuillez saisir un nombre entier.");
+                } 
+                for (int i = 1; i <= nombre; i++) { 
+
+                    factorielle *= i; 
+
+                } 
+
+                Console.WriteLine($"La factorielle de {nombre} est {factorielle}."); 
+                Console.WriteLine("Pour quitter, saisir Q. Pour continuer, appuyez sur une autre touche."); 
+                rep = Console.ReadLine(); 
+
+            } while (rep.ToUpper() != "Q");
+
+        }
+        
+    }
+}
---
+++ assurance/assurance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assurance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Dommages; 
+            double franchise; 
+            double montantRembourse; 
+
+            Console.WriteLine("Ce programme calcule le montant remboursé par l'assurance en fonction des dommages."); 
+
+            Console.WriteLine("Veuillez saisir le montant des dommages :"); 
+            Dommages = double.Parse(Console.ReadLine());
+
+
+            franchise = Dommages * 0.10; 
+            if (franchise > 4000) { 
+                franchise = 4000; 
+            } 
+
+
+            montantRembourse = Dommages - franchise; 
+
+
+            Console.WriteLine($"Montant des dommages : {Dommages} euros"); 
+            Console.WriteLine($"Franchise : {franchise} euros"); 
+            Console.WriteLine($"Montant remboursé : {montantRembourse} euros");
+
+        }
+    }
+}
